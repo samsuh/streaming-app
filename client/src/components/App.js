fcaa3f07965885d805d45cc79475b5ce340f1531
Samuel Suh <samsuh@gmail.com>
2020-05-27 02:24:11 -0700

    StreamShow implemented. CRUD finalized

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { BrowserRouter, Route } from "react-router-dom";
-import { Router, Route } from "react-router-dom"; //Router will be our own custom Router
+import { Router, Route, Switch } from "react-router-dom"; //Router will be our own custom Router. Switch matches only first successful match.
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
@@ -15,11 +15,13 @@ const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/show" exact component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
